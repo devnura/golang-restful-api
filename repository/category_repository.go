2d646b5cae94b32b617a7cf2f622ccc74bcb87f2
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2022-10-01 09:37:39 +0700

    create category repository

---
+++ repository/category_repository.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/devnura/golang-restful-api/model/domain"
+)
+
+type CategoryRepository interface {
+	Save(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
+	Update(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
+	Delete(ctx context.Context, tx sql.Tx, category domain.Category)
+	FindById(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
+	FindAll(ctx context.Context, tx sql.Tx, category domain.Category) []domain.Category
+}
