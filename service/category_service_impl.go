2ccc0adff9929dc691d4175141f4eaffe592be69
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2022-10-03 15:58:57 +0700

    Validate create and update

--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -8,14 +8,19 @@ import (
 	"github.com/devnura/golang-restful-api/model/domain"
 	"github.com/devnura/golang-restful-api/model/web"
 	"github.com/devnura/golang-restful-api/repository"
+	"github.com/go-playground/validator/v10"
 )
 
 type CategoryServiceImpl struct {
 	CategoryRepository repository.CategoryRepository
 	DB                 *sql.DB
+	Validate           *validator.Validate
 }
 
 func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
@@ -30,6 +35,9 @@ func (service *CategoryServiceImpl) Create(ctx context.Context, request web.Cate
 }
 
 func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
+
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
